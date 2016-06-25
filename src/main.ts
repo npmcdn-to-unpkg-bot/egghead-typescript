d05145911f6d8463a79854ddb6bc63d23a6f49b0
baldore <acornssn@gmail.com>
2016-06-25 13:45:37 -0500

    Add example with rxjs

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,10 @@
 import './Two';
+import * as _ from 'lodash';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+
+Observable.interval(1000)
+  .subscribe(x => console.log(x));
 
 class App {
 
